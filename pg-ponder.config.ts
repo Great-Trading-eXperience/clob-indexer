8c125a40f65a75425a025ed9411f01236f5c74a7
Bobby Fiando <bobfiando@gmail.com>
2025-05-27 20:35:40 +0700

    Configure database SSL with environment variables

--- pg-ponder.config.ts
+++ pg-ponder.config.ts
@@ -1,10 +1,19 @@
 import {createConfig} from "ponder";
 import {getBaseConfig} from "./base-ponder.config";
+import * as fs from "node:fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default createConfig({
     database: {
         kind: "postgres",
         connectionString: process.env.PONDER_DATABASE_URL,
+        poolConfig: {
+            ssl: {
+                ca: fs.readFileSync(process.env.PONDER_DATABASE_CA).toString()
+            }
+        }
     },
     ...getBaseConfig(),
 });
